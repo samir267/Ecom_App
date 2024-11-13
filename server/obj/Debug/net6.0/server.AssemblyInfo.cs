e0cebccc08bb8a5ff85e97f554088e728dc120b7
oussema69 <oussemamhatli62@gmail.com>
2024-11-13 03:37:46 -1100

    order cart

--- server/obj/Debug/net6.0/server.AssemblyInfo.cs
+++ server/obj/Debug/net6.0/server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+edc351c7f8ef8f214344bea916071559ea034980")]
 [assembly: System.Reflection.AssemblyProductAttribute("server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
